73dc77d16a16031ce8ae585eda1186632a4c9b93
Muhammad Hany <dev.mhany@gmail.com>
2025-12-31 04:06:48 +0200

    Creating .VercelIgnore to ignore Firebase functions folder

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Exclude Firebase Functions from build
+  // The functions directory is excluded via tsconfig.json and .vercelignore
 }
 
 export default nextConfig
